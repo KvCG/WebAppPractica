9362c58006d15af940d3d6b98b2366e6d13ed931
Kevin <Kevcg16@hotmail.com>
2018-05-05 18:30:30 -0600

    RouteAttr, Actions, Constraints

--- WebAppPractica/App_Start/RouteConfig.cs
+++ WebAppPractica/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace WebAppPractica
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ WebAppPractica/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppPractica.Models;
+
+namespace WebAppPractica.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { name = "L4D2" };
+            return View(movie);
+        }
+
+        public ActionResult Edit( int id) {
+            return Content("ID = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy) {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format($"pageIndex={pageIndex}&sortBy={sortBy}"));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month) { 
+        
+            return Content(year + "/" + month);
+        }
+    }
+}
